b3a1c60f71d14404e4fd046ac5285f3502b79f6e
Zach <zacharyjordan82@gmail.com>
2020-06-15 22:24:26 -0400

    adding tokens

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  adDesktopUrl:
-    "//rcm-na.amazon-adsystem.com/e/cm?o=1&p=48&l=ur1&category=primemain&banner=1FM3MKXZNJ0PPEBRV2R2&f=ifr&linkID=d20359f3bb71b9e7e5ca9556bf52e7d8&t=cooler090b-20&tracking_id=cooler090b-20",
-  adMobileUrl:
-    "//rcm-na.amazon-adsystem.com/e/cm?o=1&p=13&l=ur1&category=primemain&banner=0NVA2PKRAY588CJNAYG2&f=ifr&linkID=411da7f335f97ddb528eb51caa4a57f9&t=cooler090b-20&tracking_id=cooler090b-20",
+  adDesktopUrl: "#{adDesktopUrl}#",
+  adMobileUrl: "#{adMobileUrl}#",
 };
